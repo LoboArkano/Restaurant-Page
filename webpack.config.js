3c9e3f11945d8f9a6ac95551c2d48f8594a73a8c
LoboArkano <pipiolo_mananes@hotmail.com>
2020-08-24 18:23:40 -0500

    Install modules
    css-loader
    style-loader
    file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,24 @@ module.exports = {
     app: './src/index.js',
   },
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+    ],
   },
 };
